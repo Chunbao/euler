17c5e019c294440985c9d367f2f82aa5797c42be
Chunbao <Chunbao@users.noreply.github.com>
2015-11-13 13:14:13 +0800

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -44,10 +44,47 @@ public:
         return this;
     }
 
+    void merge(LinkedList* list) {
+        Node* p2 =  list->headPointer();
+        Node* phead = head;
+        Node* temp;
+        Node* pre = phead;
+        bool flag = false;
 
-    void reverseSection(int m, int n);
-    void merge(LinkedList& list) {
-
+        while(p2) {
+            while (phead) {
+                if(p2->val <= phead->val) { 
+                    if (head == phead) {//left insert
+                        temp = p2;
+                        p2 = p2->next;
+                        temp->next = head;
+                        phead = head;
+                        head = temp;
+                    }
+                    else { //middle insert
+                        temp = p2;
+                        p2 = p2->next;
+                        temp->next = phead;
+                        pre->next = temp;
+                    }
+                    //continue p2
+                    flag = true;
+                    break;
+                }
+                else { // no insert, continue finding
+                    pre = phead;
+                    phead= phead->next;
+                    flag = false;
+                    continue;
+                }
+            }
+            if (flag) {
+                continue;
+            }
+            // right insert
+            phead->next = p2;
+            break;
+        }
     }
 
     void sort() {
@@ -78,23 +115,75 @@ public:
         }
     }
 
+    Node* headPointer() {
+        return head;
+    }
+
+    void reverseSection(int m, int n) {
+        // check m and n are within reasonable scope
+        int reg = 0;
+        Node* p = head;
+        Node* end1 = head;
+        Node* mend = head;
+        Node* mstart = nullptr;
+        Node* temp = nullptr;
+        while (reg != n+1) {
+            ++reg;
+
+            if (reg < m-1) {
+                p = p->next;
+                continue;
+            }
+            else if(reg == m-1) {
+                end1 = p;
+                p = p->next;
+                mend = p;
+                mstart = mend;
+            }
+            else if (reg > m-1 && reg <= n) {
+                temp = p;
+                p = p->next;
+                temp->next = mstart;
+                mstart = temp;
+            }
+            else {
+                break;
+            }
+        }
+
+        if (m == 1) {
+            head = mstart;
+            mend->next = p;
+        }
+        else {
+            end1->next = mstart;
+            mend->next = p;
+        }
+    }
+
 private:
     Node* head;
 };
 
+
 int _tmain(int argc, _TCHAR* argv[])
 {
     LinkedList* m = new LinkedList;
-    m->add(0)->add(2)->add(4)->add(6)->add(8);
+    m->add(0)->add(2)->add(4)->add(6)->add(8)->add(9);
     m->sort();
     m->displayList();
+
+    // m->reverseSection(2,7);
+    // m->displayList();
 
     LinkedList* n = new LinkedList;
     n->add(1)->add(3)->add(5)->add(7)->add(9);
     n->sort();
     n->displayList();
 
-
+    m->merge(n);
+    cout << "===========" << endl;
+    m->displayList();
 
     system("PAUSE");
 	return 0;
